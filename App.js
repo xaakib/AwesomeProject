9ca60085df5df9dd2f34c9b01abe5558d847a6aa
Xaakib <64243974+xaakib@users.noreply.github.com>
2021-03-08 01:51:23 +0600

    react starting

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.Colorss}>Xvideos.com</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +14,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    color:"red",
+    backgroundColor: 'blue',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  Colorss:{
+    color:"red",
+    fontSize: "90px",
+  }
 });
